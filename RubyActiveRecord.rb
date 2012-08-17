337916a15a1b7c45bed4849b147361565b5c4458
Rebecca <hrmrebecca@gmail.com>
2012-08-16 23:41:23 -0400

    woot-woot-right file this time

--- RubyActiveRecord.rb
+++ RubyActiveRecord.rb
@@ -17,52 +17,35 @@ class Person < ActiveRecord::Base
  # self.table_name = "taxpayer"
 end
 
-#                 puts "working"
+class UserInteraction
+
 #IRB.start
   #
- #def MenuClass
-    puts "Do you want to add or edit a record?"
-    @goodanswer = ["ADD","EDIT","END"]
-    answer = gets
-    answer = answer.chomp.upcase
-    while @goodanswer.index(answer) == nil
-        puts "Please type in 'ADD' or 'EDIT' - or 'END' if you're done."
-        STDOUT.flush  
-        answer = gets
-        answer = answer.chomp.upcase
-      end
-    if answer == "ADD"
-      #c1.DataAdd
-      puts "add a record"
-    elsif answer == "EDIT"
-        #c1.DataEdit
-        puts "Whose information do you want to update?"
-        input_name = gets
-        input_name = input_name.chomp.upcase
-     #   //IRB.start
+  def starthere
+    @action = ask("Do you want to add or edit a record")
+    if %w[add edit exit].include?(@action.to_s)
+       send(@action)
+     else
+       puts "`#{@action}` is not a valid action. Please try again"
+       starthere
+     end
+  end
+  
+  def ask(question)
+    puts question
+    gets.chomp
+  end
+
+  def add
+    puts "add a record"
+  end
+  
+  def edit
+    @input_name = ask("Whose information do you need to edit?")
+    name_to_edit = Person.where(['name1 LIKE ?', "#{input_name}%"]).first
+    puts name_to_edit.name1
+  end
         
-     #   name_to_edit = Person.find(:first)
-        #name_to_edit = Person.where(:name1 => input_name).first
-        name_to_edit = Person.where(['name1 LIKE ?', "#{input_name}%"]).first
-        puts %{#{name_to_edit.inspect}}
-        
-        #puts Name.name1(name_to_edit)
-      end
+end
 
-#end
-
-#c1 = Names
-#c1.MenuClass
-
-
-# taxpayer = Names.find(name)
- # find(:first, :conditions => "NAME1 = '#{name}'")
- #Names.find_by_NAME1(name)
-  #Names.find_by_NAME1_and_NAME2(NAME1,NAME2)
- # puts "after assigning first row to names"
- # puts taxpayer.NAME1
-#end
-
-
-    # WANT TO LOOP TILL GET A USABLE ANSWER
-     
+runtime.starthere
